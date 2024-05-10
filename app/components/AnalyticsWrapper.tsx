a4e2684d0a6876040d7ee8f5a5e7ed0a774d2ede
Jayesh Jain <103871719+Jayesh-JainX@users.noreply.github.com>
2024-05-10 16:54:42 +0530

    NoteFox

--- app/components/AnalyticsWrapper.tsx
+++ app/components/AnalyticsWrapper.tsx
@@ -4,11 +4,16 @@ import { Analytics } from "@vercel/analytics/react";
 
 const AnalyticsWrapper = () => {
   const beforeSend = (event: any) => {
-    // Modify the event URL to remove the key or sensitive information
     event.url = event.url.replace(
       /\/dashboard\/new\/[a-f0-9-]+/gi,
       "/dashboard/new/"
     );
+
+    event.url = event.url.replace(
+      /\/dashboard\/show\/[a-f0-9-]+/gi,
+      "/dashboard/show/"
+    );
+
     return event;
   };
 
